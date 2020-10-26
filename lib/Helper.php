cfdb8a0c34caea75f869c55e23c90c26efce3511
Антон Н. Николаев <nikolaevanym@yamoney.ru>
2020-10-26 12:50:08 +0300

    v0.0.4

--- lib/Helper.php
+++ lib/Helper.php
@@ -3,10 +3,14 @@
 
 namespace TestYa0;
 
-
+/**
+ * Class Helper
+ * @deprecated
+ * @package TestYa0
+ */
 class Helper
 {
-    const VERSION = '0.0.3';
+    const VERSION = '0.0.4';
 
     public static function show($text)
     {
